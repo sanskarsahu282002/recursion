1683d566dff16f0067194a50b6703aae31b8f8da
sanskarsahu282002 <116749721+sanskarsahu282002@users.noreply.github.com>
2024-01-17 02:34:34 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class factorial {
+    static int fact(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        else{
+            return n*fact(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        int n=4; 
+        System.out.println(fact(n));
+    }
+}
